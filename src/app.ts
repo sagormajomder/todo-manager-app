87e58c0cded398add15fffc465d8fcb3ef40886b
Sagor Majomder <sagormajomder@gmail.com>
2026-06-25 19:15:40 +0600

    feat: update main entry point to server.ts and enhance error handling in app

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,45 @@
 import env from '@/config/env.js';
+import { globalErrorHandler } from '@/middlewares/error.middleware.js';
 import router from '@/routes/index.js';
+import { ApiError } from '@/utils/ApiError.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import type { Request, Response } from 'express';
+import type { NextFunction, Request, Response } from 'express';
 import express from 'express';
 import helmet from 'helmet';
 
 const app = express();
 
-// middlewares
+// Server Health check
+app.get('/health', (_req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'ok',
+    uptime: process.uptime(),
+  });
+});
+
+//global middlewares
+app.use(helmet());
+
 app.use(
   cors({
     origin: env.CLIENT_URL,
     credentials: true,
   }),
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(cookieParser());
 
 // Api routes
 app.use('/api/v1', router);
 
-// Server Health check
-app.get('/health', (_req: Request, res: Response) => {
-  res.status(200).json({
-    success: true,
-    message: 'Server is running',
-  });
+// 404 route
+app.use((_req: Request, _res: Response, next: NextFunction) => {
+  next(ApiError.notFound('Resource not found'));
 });
 
-// 404 route
-app.use((_req: Request, res: Response) => {
-  res.status(404).json({
-    success: false,
-    message: 'Resource not found',
-  });
-});
+// Global error handler — must be last
+app.use(globalErrorHandler);
 
 export default app;
